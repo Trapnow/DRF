3e6394ab5cbbc789b5ee572357915354bade9986
Данил <danil.arhishin@mail.ru>
2025-07-05 15:09:31 +0300

    final project

--- app/platformB/project/urls.py
+++ app/platformB/project/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 from .views import CompanyAPIView
 
 urlpatterns = [
-    path('companies/', CompanyAPIView.as_view({
-        'post': 'post',
-        'put': 'put',
-        'delete': 'delete'
-    }), name='company-operations'),
+    path('companies/', CompanyAPIView.as_view(), name='company-operations'),
 ]
