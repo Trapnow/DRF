54ea9935995c22f102a5ce13953ba28306846cdf
Данил <danil.arhishin@mail.ru>
2025-07-17 18:21:48 +0300

    final project

--- app/platformB/project/admin.py
+++ app/platformB/project/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
 
-from .models import Company, Storage, Supplier, Supply, Product
+from .models import Company, Storage, Supplier, Supply, Product, Sale, ProductSale
+
+
+class ProductSaleInline(admin.TabularInline):
+    model = ProductSale
+    extra = 1
+    readonly_fields = ('product', 'quantity')
 
 admin.site.register(Company)
 admin.site.register(Storage)
 admin.site.register(Supplier)
 admin.site.register(Supply)
 admin.site.register(Product)
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'buyer_name', 'sale_date', 'company')
+    search_fields = ('buyer_name',)
+    list_filter = ('sale_date', 'company')
+    inlines = [ProductSaleInline]
+    readonly_fields = ('id',)
+    date_hierarchy = 'sale_date'
+
+    fieldsets = (
+        (None, {
+            'fields': ('buyer_name', 'company', 'sale_date')
+        }),
+    )
 
 
